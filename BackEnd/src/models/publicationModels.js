31ed62500bf31cdfba4b376790bc27846645cd3b
Emanuel Leiva <emanueleiva12@gmail.com>
2024-10-01 13:29:59 -0300

    Modificacion de algunos modelos

--- BackEnd/src/models/publicationModels.js
+++ BackEnd/src/models/publicationModels.js
@@ -12,12 +12,31 @@ Publicacion.init({
   fecha: DataTypes.DATE,
   descripcion: DataTypes.TEXT,
   titulo: DataTypes.STRING,
-  imagen: DataTypes.STRING
+  imagen: DataTypes.STRING,
+  idUser:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Persona',   // Referencia al modelo `Persona`
+      key: 'id'      // `id` es la clave primaria de `Persona`
+    }
+  }
 }, {
   sequelize,
   modelName: 'Publicacion',
   tableName: 'publicacion',
   timestamps: false
 });
+
+// Definimos la asociación
+Persona.hasMany(Publicacion, {
+  foreignKey: 'idAutor'
+});
+
+Publicacion.belongsTo(Persona, {
+  foreignKey: 'idAutor',
+  onDelete: 'CASCADE'  // Si un usuario es eliminado, sus reseñas también lo serán
+});
+
 
 module.exports = Publicacion;
