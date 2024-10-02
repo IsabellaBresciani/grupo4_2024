6122d8ece80f7e8664e8c8f1b92969ee0061b0f1
Tomas <tomi.battis@gmail.com>
2024-10-02 16:39:31 -0300

    Search Page (simulacion-no funcional)

--- FrontEnd/src/pages/Search.jsx
+++ FrontEnd/src/pages/Search.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React , {useState} from 'react';
 import Sidebar from '../components/Sidebar';  // Asegúrate de que esta ruta sea correcta
+import ProfileCard from '../components/ProfileCard';
+
+    
 
 const Search = () => {
+    const [CantEnc, setNumber] = useState(0);
+    
+    const updateNumber = (newValue) => 
+    {
+      setNumber(Number(newValue));
+    };
+  
+    const renderComponents = () => 
+    {
+      let components = [];
+      for (let i = 0; i < CantEnc; i++) 
+        {
+        components.push(<ProfileCard key={i} />); 
+        }
+        return components;
+    };
+    
     return (
-        <div className="profile-page">
+        <div>
             <Sidebar />  {/* Mostrar solo la sidebar */}
+            <div className= "profile-content">
+              <p>Cantidad de perfiles encontrados: {CantEnc}</p>
+              <p>Ingrese cantidad de perfiles encontrados </p>
+              <input type="number" onChange={ e => updateNumber(e.target.value) } placeholder="Actualizar cantidad de perfiles encontrados" />   
+              <div>
+                {renderComponents()} 
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Search;
